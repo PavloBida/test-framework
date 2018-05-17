cd37b2cf62a0839640f1bd937f9279954a57453e
PavloBidaGL <pavlo.bida@globallogic.com>
2018-05-17 14:55:00 +0300

    Removed redundant files. Added Synchronization class. Minor general optimizations

--- src/test/java/demoqa_pagefactory/TestBase.java
+++ src/test/java/demoqa_pagefactory/TestBase.java
@@ -1,28 +1,36 @@
 package demoqa_pagefactory;
 
+import common.Synchronization;
 import common.WebDriverFactory;
 import enums.BrowserEnum;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import pageobject_demoqa.main_menu_pages.HomePage;
 
 public class TestBase {
 
-    private WebDriverFactory webDriverFactory;
+    protected Synchronization sync;
     protected WebDriver driver;
     protected HomePage homePage;
 
+    /**
+     * - Initializing WebDriver for the browser, specified in testng.xml
+     *   If no browser is specified - Chrome is used by default
+     * - Initializing HomePage for demoqa.com website
+     * - Initializing other objects, that are needed in each test
+     * @param browser
+     */
     @Parameters("browser")
     @BeforeClass
-    public void setup(BrowserEnum browser) {
-        webDriverFactory = new WebDriverFactory(browser);
-        driver = webDriverFactory.getWebdriver();
+    public void setup(@Optional("CHROME") BrowserEnum browser) {
+        driver = new WebDriverFactory(browser).getWebdriver();
+        sync = new Synchronization(driver);
         driver.get(HomePage.URL);
         homePage = PageFactory.initElements(driver, HomePage.class);
-
     }
 
     @AfterClass
