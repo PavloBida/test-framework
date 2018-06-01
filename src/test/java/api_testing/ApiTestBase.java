1144e0ba099b275c416abab0a2f596205cc6b329
Pavlo Bida <pbida@softserveinc.com>
2018-06-01 22:40:50 +0300

    Dev (#10)
    * Added screenshot util
    * Added some API tests and JSON parsing utils

---
+++ src/test/java/api_testing/ApiTestBase.java
@@ -0,0 +1,10 @@
+package api_testing;
+
+/**
+ * Created by Pavlo Bida in 2018
+ * https://github.com/PavloBida
+ */
+
+public class ApiTestBase {
+    public static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+}
---
+++ src/test/java/api_testing/ParseJson.java
@@ -0,0 +1,33 @@
+package api_testing;
+
+import dto.CommentsDTO;
+import dto.PostDTO;
+import org.testng.annotations.Test;
+import util.JsonUtil;
+
+import java.util.List;
+
+/**
+ * Created by Pavlo Bida in 2018
+ * https://github.com/PavloBida
+ */
+
+public class ParseJson {
+
+    @Test
+    public void testJson() {
+        List<CommentsDTO> comments =
+                new JsonUtil<CommentsDTO>().parseJson("src/test/resources/comments.json", CommentsDTO.class);
+
+        comments.forEach(System.out::println);
+    }
+
+    @Test
+    public void testAnotherJson() {
+        List<PostDTO> comments =
+                new JsonUtil<PostDTO>().parseJson("src/test/resources/posts.json", PostDTO.class);
+
+        comments.forEach(System.out::println);
+    }
+
+}
